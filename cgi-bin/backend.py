06d1fd8bb0513422a3213c0424e7922c374d2af3
João Guilherme Di Foggi Luchetti <joaodifoggi@hotmail.com>
2017-11-05 17:36:20 -0200

    Alterações backend.py
    -Retorna a informação que a máquina recusou conexão (possivelmente desligada)
    -Informações sobre a máquina agora ficam em webserver.py (como diz a descrição de VM-desenvolvimento.pdf)

--- cgi-bin/backend.py
+++ cgi-bin/backend.py
@@ -1,15 +1,17 @@
 import socket 
 import pacotes
 
-def enviaComando(maquina, comando, parametros):
+def enviaComando(endereco, porta, comando, parametros):
 	ipOrigem = '127.0.0.1' #loopback
 	ipDestino = '127.0.0.1'
-	portas = [8000, 8001, 8002] #portas para comunicacao com cada maquina
 	
-	addr = ('localhost' ,portas[maquina])
+	addr = (endereco,porta)
 	cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	cliente.connect(addr) 
-	
+	try: #tentando conectar a maquina
+		cliente.connect(addr) 
+	except socket.error:
+		return 'Conexao recusada!\nTalvez a maquina esteja desligada'
+		
 	#Codificando e enviando pacote
 	packet = pacotes.codificaPacote(comando,parametros,ipOrigem,ipDestino,'000','00001111',0)
 	cliente.send(packet) 
