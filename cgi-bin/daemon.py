0b60f59ed07c12c6a0801ff753b24ff38992315a
brunoinfo <bruno.cezar3000@gmail.com>
2017-11-05 13:27:12 -0200

    daemon.py multithreaded
    Versão multithreaded do arquivo daemon.py

--- cgi-bin/daemon.py
+++ cgi-bin/daemon.py
@@ -2,12 +2,14 @@ import socket
 import argparse
 import pacotes
 import subprocess
+import threading
 
 #funcao para executar comando
 def executaComando(comando,argumentos):
 	#Validando para verificar se nao existem parametros maliciosos nos argumentos adicionais
 	if('|' in argumentos or ';' in argumentos or '>' in argumentos):
 		return 'Existem parametros maliciosos nesta requisicao'
+	
 	
 	#Executando o comando
 	try:
@@ -16,31 +18,48 @@ def executaComando(comando,argumentos):
 		return "Comando invalido"
 	return output
 	
-#Referencia https://pythonhelp.wordpress.com/2011/11/20/tratando-argumentos-com-argparse/
-parser = argparse.ArgumentParser()
-parser.add_argument('--port', action='store', type=int, dest='port', required=True, help='Porta em qual o daemon.py ira rodar')
-args = parser.parse_args()
+#Referencia https://stackoverflow.com/questions/23828264
+class ThreadedServer(object):
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port 
+		addr = (host, port) 
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.bind(addr)
+		
+	def listen(self):
+		self.socket.listen(5)
+		while True:
+			cliente, endereco = self.socket.accept()
+			cliente.settimeout(5)
+			threading.Thread(target = self.listenToClient,args = (cliente,endereco)).start()
+
+	def listenToClient(self, cliente, endereco):
+		while True:
+			try:
+				#Recebendo o pacote
+				packet = cliente.recv(1024)
+				if packet:
+					#Decodificando pacote
+					print 'Requisicao recebida'
+					comando,parametros,ipOrigem,ipDestino,ttl,id = pacotes.decodificaComandoPacote(packet);
+					print 'Executando comando ' + comando + ' ' + parametros 
+					resposta = executaComando(comando,parametros)
+					
+					#Enviando a resposta
+					packet = pacotes.codificaPacote(comando, resposta, ipOrigem, ipDestino, '111', ttl, id)
+					cliente.send(packet)
+					print 'Resposta enviada!'
+				else:
+					raise error('Cliente desconectado')
+			except:
+				cliente.close()
+				return False
+
+if __name__ == "__main__":
+	#Referencia https://pythonhelp.wordpress.com/2011/11/20/tratando-argumentos-com-argparse/
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--port', action='store', type=int, dest='port', required=True, help='Porta em qual o daemon.py ira rodar')
+	args = parser.parse_args()
 	
-#criando o socket
-host = 'localhost' 
-port = int(args.port) 
-addr = (host, port) 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp.bind(addr) 
-tcp.listen(1) 
-while True:
-	print 'Aguardando conexao na porta ' + str(port)
-	conn, cliente = tcp.accept() 
-	
-	#Recebendo o pacote
-	packet = conn.recv(1024) 
-	print 'Requisicao recebida'
-	comando,parametros,ipOrigem,ipDestino,ttl,id = pacotes.decodificaComandoPacote(packet);
-	print 'Executando comando ' + comando + ' ' + parametros 
-	resposta = executaComando(comando,parametros)
-	
-	#Enviando a resposta
-	packet = pacotes.codificaPacote(comando, resposta, ipOrigem, ipDestino, '111', ttl, id)
-	conn.send(packet)
-	print 'Resposta enviada!'
-tcp.close()
+	ThreadedServer('localhost',int(args.port)).listen()
