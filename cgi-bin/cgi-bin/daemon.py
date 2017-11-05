249b6455a7c5f8f2b0e015a8a7007a54419dbe82
brunoinfo <bruno.cezar3000@gmail.com>
2017-11-05 15:21:10 -0200

    Melhorias em daemon.py
    Identificação de qual porta é feita a requisição.

---
+++ cgi-bin/cgi-bin/daemon.py
@@ -0,0 +1,65 @@
+import socket 
+import argparse
+import pacotes
+import subprocess
+import threading
+
+#funcao para executar comando
+def executaComando(comando,argumentos):
+	#Validando para verificar se nao existem parametros maliciosos nos argumentos adicionais
+	if('|' in argumentos or ';' in argumentos or '>' in argumentos):
+		return 'Existem parametros maliciosos nesta requisicao'
+	
+	#Executando o comando
+	try:
+		output = subprocess.check_output(comando + ' ' + argumentos, shell=True)
+	except subprocess.CalledProcessError:                                                                                                   
+		return "Comando invalido"
+	return output
+	
+#Referencia https://stackoverflow.com/questions/23828264
+class ThreadedServer(object):
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port 
+		addr = (host, port) 
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.bind(addr)
+		
+	def listen(self):
+		self.socket.listen(5)
+		while True:
+			cliente, endereco = self.socket.accept()
+			cliente.settimeout(5)
+			threading.Thread(target = self.listenToClient,args = (cliente,endereco)).start()
+
+	def listenToClient(self, cliente, endereco):
+		while True:
+			try:
+				#Recebendo o pacote
+				packet = cliente.recv(1024)
+				if packet:
+					#Decodificando pacote
+					print 'Requisicao recebida na porta ' + str(self.port)
+					comando,parametros,ipOrigem,ipDestino,ttl,id = pacotes.decodificaComandoPacote(packet);
+					print 'Executando comando ' + comando + ' ' + parametros 
+					resposta = executaComando(comando,parametros)
+					
+					#Enviando a resposta
+					packet = pacotes.codificaPacote(comando, resposta, ipOrigem, ipDestino, '111', ttl, id)
+					cliente.send(packet)
+					print 'Resposta enviada!'
+					print ''
+				else:
+					raise error('Cliente desconectado')
+			except:
+				cliente.close()
+				return False
+
+if __name__ == "__main__":
+	#Referencia https://pythonhelp.wordpress.com/2011/11/20/tratando-argumentos-com-argparse/
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--port', action='store', type=int, dest='port', required=True, help='Porta em qual o daemon.py ira rodar')
+	args = parser.parse_args()
+	
+	ThreadedServer('localhost',int(args.port)).listen()
