6881d018c6ac6b3a497fd7d3af9ac0b88442e829
brunoinfo <bruno.cezar3000@gmail.com>
2017-11-06 21:51:35 -0200

    Script run.sh
    Script para execução do programa daemon.py nas protas 8000, 8001 e 8002.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python /usr/lib/cgi-bin/daemon.py --port 8000 &
+python /usr/lib/cgi-bin/daemon.py --port 8001 &
+python /usr/lib/cgi-bin/daemon.py --port 8002 &
+echo "Executando maquinas nas portas 8000, 8001 e 8002."
